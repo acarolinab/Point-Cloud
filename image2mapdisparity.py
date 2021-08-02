7d464c6003d9f3e42d5380f29e07124ee3aad956
Senai <anacarolinab4@gmail.com>
2021-08-01 21:40:19 -0300

    RGB image 2 map disparity

---
+++ image2mapdisparity.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+imgL = cv2.imread('/home/senai/Documentos/Master/Dataset/Left_Unrealrgb_00000.png',0)
+imgR = cv2.imread('/home/senai/Documentos/Master/Dataset/Right_Unrealrgb_00000.png',0)
+
+stereo = cv2.StereoBM_create(numDisparities=16, blockSize=5)
+
+disparity = stereo.compute(imgL,imgR)
+
+plt.imshow(disparity,'gray')
+
+plt.show() 
