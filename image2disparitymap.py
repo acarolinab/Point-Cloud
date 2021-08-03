64faa7bf23aea435424c8421d22f19f2bd5f1c4a
Senai <anacarolinab4@gmail.com>
2021-08-03 12:17:37 -0300

    Add 2 arg - path image;

--- image2disparitymap.py
+++ image2disparitymap.py
@@ -1,9 +1,23 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+import argparse
 
-imgL = cv2.imread('/home/senai/Documentos/Master/Dataset/Left_Unrealrgb_00000.png',0)
-imgR = cv2.imread('/home/senai/Documentos/Master/Dataset/Right_Unrealrgb_00000.png',0)
+
+ap = argparse.ArgumentParser( description='Image RGB')
+ap.add_argument("-l", 
+    "--imageL", required=True,
+	help="path to the left image file")
+ap.add_argument("-r",
+    "--imageR", required=True,
+    help="path to the right image file")
+args = vars(ap.parse_args())
+
+
+imgL = cv2.imread(args['imageL'],0)
+imgR = cv2.imread(args['imageR'],0)
+
+
 
 """ stereo = cv2.StereoBM_create(numDisparities=16, blockSize=5)
 
@@ -18,7 +32,7 @@ min_disp = -1
 max_disp = 63 #min_disp * 9
 num_disp = max_disp - min_disp # Needs to be divisible by 16
 
-#Create Block matching object. 
+#Block matching. 
 stereo = cv2.StereoSGBM_create(minDisparity= min_disp,
 numDisparities = num_disp,
 blockSize = 5,
@@ -33,6 +47,5 @@ P2 = 32*3*win_size**2) #32*3*win_size**2)
 print ("\nComputing the disparity  map...")
 disparity_map = stereo.compute(imgL, imgR)
 
-#Show disparity map before generating 3D cloud to verify that point cloud will be usable. 
 plt.imshow(disparity_map,'gray')
 plt.show()
